fd297df19ac88fe99b4399c49483057166814e6f
aman <amansoni22103@gmail.com>
2024-06-20 08:53:02 +0530

    added password update method

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,58 +1,53 @@
 const userModel = require("../models/userModel");
 const bcrypt = require('bcryptjs');
 const JWT = require("jsonwebtoken");
+
 //Register
-const registerController = async(req, res) => {
-    try{
-        const {userName, email, password, phone, address} = req.body;
-        //validation
-        if(!userName || !email || !password || !phone || !address ){
-            return res.status(500).send({
-                success: false,
-                message: "Please Provide All Fields",
-            });
-        }
-        //check user
-        const existing = await userModel.findOne({email})
-        if(existing){
-            return res.status(500).send({
-                success: false,
-                message: 'Email Already Registered Please Loging',
-            });
-        }
-        //check user password  | compare password
-        const isMatch = await bcrypt.compare(password, user.password);
-        if (!isMatch) {
-            return res.status(500).send({
-                success: false,
-                message: "Invalid Credentials",
-            });
-        }
-        //Hashing password
-        var salt = bcrypt.genSaltSync(10);
-        const hashedPassword = await bcrypt.hash(password, salt)
-        
-        //create new user
-        const user =  await userModel.create({
-            userName, 
-            email,
-            password: hashedPassword, 
-            address, 
-            phone
-        })
-        res.status(201).send({
-            success: true,
-            message: 'Successfully Registered',
+const registerController = async (req, res) => {
+    try {
+      const { userName, email, password, phone, address, answer } = req.body;
+      //validation
+      if (!userName || !email || !password || !address || !phone || !answer) {
+        return res.status(500).send({
+          success: false,
+          message: "Please Provide All Fields",
         });
-    }catch(error){
-        console.log(error);
-        res.status(500).send({
-            success:false,
-            message: 'Error in Register API',
-            error
-        })
+      }
+      // chekc user
+      const exisiting = await userModel.findOne({ email });
+      if (exisiting) {
+        return res.status(500).send({
+          success: false,
+          message: "Email Already Registerd please Login",
+        });
+      }
+      //hashing password
+      var salt = bcrypt.genSaltSync(10);
+      const hashedPassword = await bcrypt.hash(password, salt);
+      //create new user
+      const user = await userModel.create({
+        userName,
+        email,
+        password: hashedPassword,
+        address,
+        phone,
+        answer,
+      });
+      res.status(201).send({
+        success: true,
+        message: "Successfully Registered",
+        user,
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(500).send({
+        success: false,
+        message: "Error In Register API",
+        error,
+      });
     }
-};
+  };
+  
 
 //Login
 const loginController = async(req, res) => {
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,11 +1,27 @@
 const express = require('express');
-const { getUserController } = require('../controllers/userConntroller');
+const { getUserController } = require('../controllers/userController');
 const authMiddleware = require('../middlewares/authMiddleware');
+const { updateUserController } = require('../controllers/userController');
+const { updatePasswordController } = require('../controllers/userController');
+const { resetPasswordController } = require('../controllers/userController');
+const { deleteProfileController } = require('../controllers/userController');
 
 const router  = express.Router()
 
 //routes
 //Get User || post
 router.get('/getUser', authMiddleware, getUserController)
+
+//update profile
+router.put('/updateUser', authMiddleware, updateUserController )
+
+//update user password
+router.post('/updatePassword',authMiddleware,updatePasswordController)
+
+//reset password
+router.post('/resetPassword',authMiddleware, resetPasswordController)
+
+// delete USER
+router.delete("/deleteUser/:id", authMiddleware, deleteProfileController);
 
 module.exports = router
